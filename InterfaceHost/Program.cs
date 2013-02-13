e859eb07515d76289351cfef4d65fd10f54fcfe1
Drew Ogle <daogle@proquobooks.com>
2013-02-13 11:49:41 -0500

    modify commandline arguments to make hosting url configurable.

--- InterfaceHost/Program.cs
+++ InterfaceHost/Program.cs
@@ -15,10 +15,18 @@ using OldQuick.Inventory;
 
 namespace InterfaceHost
 {
+    // Because ServiceStack uses the .NET Web Hosting code on windows, 
+    // this may require a netssh http add urlacl url=http://+:8080/my_dll/ user=\Everyone by an Administrator
     public class Program
     {
         static void Main(string[] args)
         {
+            if (args.Count() != 2)
+            {
+                Console.WriteLine("InterfaceHosh: argument error. Format is <dll> <hosting_url>");
+                Console.WriteLine("dll can be relative or absolute path, hosting_url is MS hosting url style ie: http://+:88/my_dll/");
+                Environment.Exit(1);
+            }
             DllLoader loader = new DllLoader(args[0]);
             OldQuickInventoryRepositoryHostable h = loader.host.GetConstructor(new Type[] { }).Invoke(null) as OldQuickInventoryRepositoryHostable;
             Dictionary<string, Type> shortname_map;
@@ -35,12 +43,28 @@ namespace InterfaceHost
                 if (ie is ReflectionTypeLoadException)
                 {
                     Exception[] le = ((ReflectionTypeLoadException)ie).LoaderExceptions;
+                    Console.WriteLine("Error loading dll for hosting: : " + e.ToString());
+                    Console.WriteLine("Loader exceptions are:" + le.Select( ile => ile.ToString()).Aggregate( (f,s) => f + "\n" + s ));
                 }
+                else
+                {
+                    Console.WriteLine("Error while initializing hosting: " + e.ToString());
+                }
+                Environment.Exit(2);
             }
-            ssh.Start("http://+:8088/joinus/");
+            try
+            {
+                ssh.Start(args[1]);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error starting Hosting: " + e.ToString());
+                Environment.Exit(3);
+            }
             Console.WriteLine("ServiceStack-based InterfaceHost v0.0.1, Serving " + service_name);
             Console.WriteLine("Press <enter> to terminate.");
             Console.ReadLine();
+            Environment.Exit(0);
         }
 
     }
