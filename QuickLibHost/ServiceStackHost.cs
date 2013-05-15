dcbe37f29cfbcc2e5d7d32f651ff670516cbde15
Joshua Hofmann <jhofmann@discoverbooks.com>
2013-05-15 11:32:07 -0400

    Apply LGPL license.

--- QuickLibHost/ServiceStackHost.cs
+++ QuickLibHost/ServiceStackHost.cs
@@ -1,4 +1,23 @@
-﻿using QuickLibHostClient;
+﻿/* 
+ * Copyright (C) 2013 the QuickLibHost contributors. All rights reserved.
+ * 
+ * This file is part of QuickLibHost.
+ * 
+ * QuickLibHost is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * QuickLibHost is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with QuickLibHost.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using QuickLibHostClient;
 using ServiceStack.Logging.Support.Logging;
 using ServiceStack.WebHost.Endpoints;
 using System;
